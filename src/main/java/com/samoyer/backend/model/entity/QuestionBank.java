3e4e2ee5e03f1f974466dc7a6c4b410f759209b2
xuzhichao <x_zhichao@outlook.com>
2024-10-14 23:01:57 +0800

    引入Druid连接池，提升系统性能和响应速度

--- src/main/java/com/samoyer/backend/model/entity/QuestionBank.java
+++ src/main/java/com/samoyer/backend/model/entity/QuestionBank.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.*;
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
+import org.springframework.data.annotation.Version;
 
 /**
  * 题库
